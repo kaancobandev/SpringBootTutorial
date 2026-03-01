23f83066d4e2fa9fed399be8fa6239e29c065362
Kaan Çoban <kaaan3452@gmail.com>
2026-03-01 21:32:30 +0300

    Bean Lifecycle: @PostConstruct and @PreDestroy

---
+++ movie-reccomender-system/src/main/java/org/kaan/moviereccomendersystem/package11/ContentBasedFilter.java
@@ -0,0 +1,35 @@
+package org.kaan.moviereccomendersystem.package11;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ContentBasedFilter implements Filter {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    public ContentBasedFilter() {
+        super();
+        logger.info("In ContentBasedFilter constructor method");
+    }
+
+    @PostConstruct
+    private void postConstruct(){
+        //load movies into cache
+        logger.info("In ContentBasedFilter postConstruct method");
+    }
+
+    @PreDestroy
+    private void preDestroy(){
+        //clear movies from cache
+        logger.info("In ContentBasedFilter preDestroy method");
+    }
+
+    //getRecommendations takes a movie as input and returns a list of similar movies
+    public String[] getRecommendations(String movie) {
+        //logic of content based filter
+        return new String[]{"Happy Feet", "Ice Age", "Shark Tale"};
+    }
+}
---
+++ movie-reccomender-system/src/main/java/org/kaan/moviereccomendersystem/package11/RecommenderImplementation.java
@@ -0,0 +1,42 @@
+package org.kaan.moviereccomendersystem.package11;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RecommenderImplementation {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    //use filter interface to select filter
+    private Filter filter;
+
+    @Autowired
+    public void setFilter(Filter filter){
+        logger.info("In RecommenderImplementation setter method.. dependency injection");
+        this.filter = filter;
+    }
+
+    @PostConstruct
+    public void postConstruct(){
+        //initialization code goes here
+        logger.info("In RecommenderImplementation postConstruct method");
+    }
+
+    @PreDestroy
+    public void preDestroy(){
+        //cleanup code
+        logger.info("In RecommenderImplementation preDestroy method");
+    }
+
+    public String[] recommendMovies(String movie){
+        //print the name of interface implementation being used
+        System.out.println("Name of the filter in use: " + filter + "\n");
+        String[] results = filter.getRecommendations("Finding Dory");
+
+        return results;
+    }
+}
